ec407f2b8a600c942e96cff764b1a1c9e4cc35e3
Sayak-77-sys <s3151855@gmail.com>
2026-03-02 21:32:27 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './App.css';
+import AppLayout from './layouts/app-layout';
+import LandingPage from './pages/landing';
+import Onboarding from './pages/onboarding';
+import JobPage from './pages/job';
+import JobListing from './pages/job-listing';
+import MyJobs from './pages/my-jobs';
+import PostJob from './pages/post-job';
+import SavedJobs from './pages/saved-jobs';
+import { ThemeProvider } from './components/theme-provider';
+import ProtectedRoute from './components/protected-route';
+
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <LandingPage />,
+      },
+      {
+        path: "/onboarding",
+        element: (
+          <ProtectedRoute>
+            <Onboarding />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/job/:id",
+        element: (
+          <ProtectedRoute>
+            <JobPage />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/jobs",
+        element: (
+          <ProtectedRoute>
+            <JobListing />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/my-jobs",
+        element: (
+          <ProtectedRoute>
+            <MyJobs />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/post-job",
+        element: (
+          <ProtectedRoute>
+            <PostJob />
+          </ProtectedRoute>
+        ),
+      },
+      {
+        path: "/saved-jobs",
+        element: (
+          <ProtectedRoute>
+            <SavedJobs />
+          </ProtectedRoute>
+        ),
+      },
+    ],
+  },
+]);
+
+
+function App() {
+  
+
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
+}
+
+export default App
---
+++ src/layouts/app-layout.jsx
@@ -0,0 +1,20 @@
+
+import Header from "@/components/header";
+import { Outlet } from "react-router-dom";
+
+const AppLayout = () => {
+  return (
+    <div>
+      <div className="grid-background"></div>
+      <main className="min-h-screen container px-20">
+        <Header/>
+        <Outlet />
+      </main>
+      <div className="p-10 text-center bg-gray-800 mt-10">
+        Made with 💗 by Sayak
+      </div>
+    </div>
+  );
+};
+
+export default AppLayout;
